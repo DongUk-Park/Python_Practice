e524cd2dcdbc87d2ae4a81a23e4dd89d92b192c1
DongUk-Park <56333631+DongUk-Park@users.noreply.github.com>
2022-05-04 09:21:53 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name='mycalc001',
+  version='1.0.0',
+  description='oss-dev my calculator test 0001',
+  author='DongukPark',
+  author_email='parkdw00@naver.com',
+  url = "https://github.com/DongUk-Park/Caculator/",
+  download_url = "",
+  install_requires=['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+
+)
